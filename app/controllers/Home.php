a3889dc61225fd71026c60d9633fe07c0ad19194
Raj-Lov <rajeshnadupuru98@gmail.com>
2025-01-21 06:18:25 +0530

    Update Home.php

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,14 +2,7 @@
 
     class Home extends Controller {
         public function index(){
-            $db = new Database();
-            $res = $db->query(("select * from users"));
-
-            show($res);
-            
-            $users = new Users();
-            $users -> insert($data);
-
+       
             $data['title'] = 'Home';
             $this->view('home',$data);
         }
